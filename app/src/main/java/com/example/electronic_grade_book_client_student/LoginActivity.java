16c87b5d128633613d38bc9dcf9f9adcb36f85b7
Michal <lasek3@gmail.com>
2019-12-11 18:07:27 +0100

    Change design of LoginActivity

--- app/src/main/java/com/example/electronic_grade_book_client_student/LoginActivity.java
+++ app/src/main/java/com/example/electronic_grade_book_client_student/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +25,7 @@ import java.util.Collections;
 public class LoginActivity extends AppCompatActivity {
 
     private Button singInButton;
+    private TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -70,6 +72,9 @@ public class LoginActivity extends AppCompatActivity {
 
             requestHeaders.setAuthorization(authHeader);
             requestHeaders.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+            System.out.println("auth: "+ authHeader);
+            System.out.println("request: "+requestHeaders);
 
             RestTemplate restTemplate = new RestTemplate();
             restTemplate.getMessageConverters().add(new MappingJacksonHttpMessageConverter());
